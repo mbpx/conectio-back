1bef557584ff46e09aa6503400ac88efb2e0677b
Miguel Bautista <bautistaperez3m@gmail.com>
2023-10-30 17:41:19 +0100

    Cors config

--- src/main/java/com/amx/conectio/config/ApplicationConfig.java
+++ src/main/java/com/amx/conectio/config/ApplicationConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.amx.conectio.user.UserRepository;
 
@@ -46,4 +48,13 @@ public class ApplicationConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/auth/authenticate").allowedOrigins("http://localhost:8100");
+			}
+		};
+	}
 }
